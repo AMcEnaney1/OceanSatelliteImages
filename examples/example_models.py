6ae0ba6bc68d9cb1d16862216a3f0d509ac66762
Aidan McEnaney <aidan.mcenaney@gmail.com>
2023-08-16 21:40:02 -0400

    model example implemented

--- examples/example_models.py
+++ examples/example_models.py
@@ -6,9 +6,7 @@ Date: 2023-08-11
 Description: Python file for examples in basic functionality of the SentinelHub API with the project.
 
 Contents:
-    - basic_download_single: Example function for how to download a singular band of data.
-    - basic_download_multi: Example function for how to download multiple bands of data.
-    - multiple_locations: Example function for how to download from multiple locations in one function.
+    - download_and_apply_model: Example function for calling linear model.
 
 Notes:
     - This code is distributed under the MIT License. See LICENSE.txt for more details.
@@ -16,36 +14,47 @@ Notes:
 
 # Standard library imports
 import os
+from datetime import datetime
 
 # Local module imports
-import local_sentinelhub.sentinelhub_manage_functions as shm
 import utils.misc_functions as mf
-import local_sentinelhub.requestFunctions as rf
-import utils.polymer_functions as pf
+import local_sentinelsat.request_functions as rf
+import utils.model_application_functions as maf
+import models.model_functions as mmf
+from config import *
 
-def download_and_apply():
+def download_and_apply_model():
     """
-    Example of applying POLYMER to a snapshot downloaded using the sentinelsat API.
+    Example of applying a parameter model to a snapshat from the sentinelsat API.
     This code to download the data is from 'example_sentinelsat.py'.
     """
 
+    # Settings
     coordinates = (-69.9040, 43.8586, -69.8987, 43.8651)
+    start_year, end_year = 2023, 2023
+    start_month, end_month = 2, 7
+    start_day, end_day = 1, 30
+    n_chunks = 3
+
+    project_name = 'example_model'
+    npy_save_to = project_name + '_' + 'poly_files'
+
+    # Date calculation
+    start = datetime(start_year, start_month, start_day)
+    end = datetime(end_year, end_month, end_day)
+    date_tuples = mf.get_timeslots(start, end, n_chunks)
 
     # File path
     file_paths_dict = {
-        'download_dir': 'out/'
+        'poly_dir': os.path.join(polymer_path, 'chlorPoly', project_name) + '/'
     }
-    mf.make_absolute_paths_dict(file_paths_dict)
 
-    date_tuples = [('2023-03-01', '2023-03-14')]
+    # /Users/aidan/Documents/secondGit/copy_prior_branch_OceanSatelliteImages/polymer-v4.16.1/chlorPoly/MaxFarm
+    # MaxFarm_poly_files
 
-    shm.sentinelsat_routine(
-        coordinates, date_tuples, file_paths_dict['download_dir'], request_function=rf.get_olci_singular
-    )
+    maf.model_routine_space_eff(coordinates, date_tuples, project_name, mmf.chlor,
+                                file_paths_dict['poly_dir'], request_function=rf.get_olci_singular,
+                                npy_save_to=npy_save_to)
 
-    filevals = mf.get_surface_level_folders(file_paths_dict['download_dir'])  # Gets a list of the downloaded folder(s)
-    filevals = [os.path.join(file_paths_dict['download_dir'], file_path) for file_path in filevals]
-
-    folder = mf.most_recent_folder(filevals) # Getting the newest folder, this should be the one we just downloaded
-
-    pf.call_polymer(folder) # Now we call the polymer script
+if __name__ == "__main__":
+    download_and_apply_model()
