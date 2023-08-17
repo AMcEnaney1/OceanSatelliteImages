1eed0eab96b2819df4a586b88f68b21f5fbe13aa
Aidan McEnaney <aidan.mcenaney@gmail.com>
2023-08-17 15:22:50 -0400

    added

---
+++ conf/keys.py
@@ -0,0 +1,11 @@
+## Aidan McEnaney
+## June 30th, 2023
+## Python file to store keys
+
+# Keys for the SentinelHub API
+client_id = ''
+client_secret = ''
+
+# Keys for the Sentinelsat API
+username_sensat = ''
+password_sensat = ''
